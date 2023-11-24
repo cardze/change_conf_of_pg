220d5922814c94684294d1a73a85e234cbe0e3a7
cardze <43008882+cardze@users.noreply.github.com>
2023-11-24 14:45:51 +0800

    add: conda env yaml

--- test_bash/clean_pg_cache.sh
+++ test_bash/clean_pg_cache.sh
@@ -1,5 +1,6 @@
+#! /bin/bash
 systemctl stop postgresql.service
 sync
 echo '3' | sudo /usr/bin/tee -a /proc/sys/vm/drop_caches
 systemctl start postgresql.service
-service tomcat9 restart
+# service tomcat9 restart
